c0e6d62f2b19648cf1008b9bf7ab373b1705d1cc
sm-shah <39312127+sm-shah@users.noreply.github.com>
2020-01-13 03:42:41 +0530

    update pro env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD4kn9oYrIe-vdUhxSRXwhQLneZ_c1AhfQ",
+    authDomain: "pokemon-8b299.firebaseapp.com",
+    databaseURL: "https://pokemon-8b299.firebaseio.com",
+    projectId: "pokemon-8b299",
+    storageBucket: "pokemon-8b299.appspot.com",
+    messagingSenderId: "963867697285",
+    appId: "1:963867697285:web:b51663b21ac3fc962cc9ec",
+    measurementId: "G-SQ3PZHV4EL"
+  }
 };
